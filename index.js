d75e43f4324daabe3c098fb9cc71219161295ece
fahim-42 <you@example.com>
2022-08-19 11:47:09 +0600

    localhost link commented

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.use(fileUpload());
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
+/*localhost*/
+// const uri = 'mongodb://127.0.0.1:27017';
+
+/*cloud*/
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p55ig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
